6732a9d17e0704959d84b171355d1c93544e429e
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2023-11-10 16:16:48 -0400

    Creamos nuesta clase Tickets

---
+++ TicketDetalle/Shared/Tickets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketDetalle.Shared
+{
+    internal class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+        public DateTime Fecha {  get; set; }
+        [Required(ErrorMessage ="Favor completar este campo")]
+        public string? SolicitadoPor {  get; set; }
+        [Required(ErrorMessage = "Favor completar este campo")]
+        public string? Asunto { get; set; }
+        [Required(ErrorMessage = "Favor completar este campo")]
+        public string? Descripcion { get; set; }
+
+    }
+}
