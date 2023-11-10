36d6fc68139053789169caed2fbed257ab2fcf08
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2023-11-10 16:31:40 -0400

    Inyectamos nuestro contex en el program

--- TicketDetalle/Server/Program.cs
+++ TicketDetalle/Server/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using TicketDetalle.Server.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Context>(op => op.UseSqlite(ConStr));
 
 var app = builder.Build();
 
