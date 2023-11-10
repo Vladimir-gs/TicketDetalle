a8c95afd1eb947f60d35f13c265c68c9aefe728a
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2023-11-10 16:25:34 -0400

    Creamos nuestro Context

---
+++ TicketDetalle/Server/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketDetalle.Shared;
+
+namespace TicketDetalle.Server.DAL
+{
+    public class Context : DbContext
+    {
+        public DbSet<Tickets> Tickets { get; set; }
+
+        public Context(DbContextOptions<Context> options)
+            :base(options){ }
+    }
+}
